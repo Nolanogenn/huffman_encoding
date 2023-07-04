46edd1e05648fde823f59ac643ea020cd258d0d0
Nolanogenn <gnolano@LINUP130.MareGroup.local>
2023-07-04 14:56:38 +0200

    initial commit

--- get_huffman.pl
+++ get_huffman.pl
@@ -1,19 +1,27 @@
+use Tree::Simple;
 use warnings;
 use strict;
 
 my $filename = $ARGV[0];
 open(FH, '<', $filename) or die $!;
-my @tokens;
+my @tokens = ();
 my @linetokens;
+my %token_hash;
+my $huffmann_tree = Tree::Simple->new("0", Tree::Simple->ROOT); 
 
 while(<FH>){
 	my $line = $_;
 	my @linetokens = split(' ', $line);
 	foreach my $token (@linetokens){
 		my $lc_token = lc $token;
-		print "$lc_token\n";
+		push(@tokens, $lc_token);
 }
-}
+};
+foreach my $token (@tokens) {
+	$token_hash{$token}++
+};
+
+%sorted_tokens = reverse sort values %token_hash;
 
 
 
